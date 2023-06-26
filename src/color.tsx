8336b28a14c039714c5e593cc05fa04e652e1e35
Daniel  Hernandez <hernandezdany41@gmail.com>
2023-06-26 09:45:39 -0700

    agregando primer configuracion (carpetas e iconos)

---
+++ src/color.tsx
@@ -0,0 +1,13 @@
+const Colors = {
+  primary: "#FF6900",
+  danger: "#FF0000",
+  warning: "#FFCC00",
+  blue: "#267ED6",
+  dark: "#212529",
+  gray: "#6C757D",
+  lightGray: "#F8F9FA",
+  beige: "#F8E9D4",
+  white: "#FFFFFF",
+};
+
+export default Colors;
